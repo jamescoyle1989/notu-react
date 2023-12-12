e4c1ff9ff88f1bdd220f17831198ebd6b2070bfb
James Coyle <jamescoyle1989@gmail.com>
2023-12-11 22:02:45 -0500

    Added SpaceEditor

---
+++ src/SpaceEditor.tsx
@@ -0,0 +1,56 @@
+import { Space } from 'notu';
+import { useState } from 'react';
+import 'purecss';
+import './SpaceEditor.css';
+
+interface SpaceEditorProps {
+    space: Space,
+    onConfirm: () => string
+}
+
+
+export const SpaceEditor = ({
+    space,
+    onConfirm
+}: SpaceEditorProps) => {
+
+    const [name, setName] = useState(space.name);
+    const [error, setError] = useState(null);
+
+    function onNameChange(event): void {
+        setName(event.target.value);
+        space.name = event.target.value;
+    }
+
+    function onConfirmClick() {
+        const errorMessage = onConfirm();
+        setError(errorMessage ?? null);
+    }
+
+    function renderErrorMessage() {
+        if (!error)
+            return;
+        return (
+            <div className="errorBanner">
+                <label>Error: {error}</label>
+            </div>
+        );
+    }
+
+    return (
+        <form className="pure-form pure-form-aligned">
+            <fieldset>
+                {renderErrorMessage()}
+
+                <div className="pure-control-group">
+                    <label>Name</label>
+                    <input type="text" onChange={onNameChange}/>
+                </div>
+
+                <div className="pure-controls">
+                    <button type="button" onClick={onConfirmClick} className="pure-button pure-button-primary">Confirm</button>
+                </div>
+            </fieldset>
+        </form>
+    );
+}
---
+++ src/stories/SpaceEditor.stories.ts
@@ -0,0 +1,38 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { SpaceEditor } from '../SpaceEditor';
+import { Space } from 'notu';
+
+const meta = {
+    title: 'SpaceEditor',
+    component: SpaceEditor,
+    parameters: {
+        layout: 'padded'
+    },
+    tags: ['autodocs']
+} satisfies Meta<typeof SpaceEditor>;
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+
+
+export const Primary: Story = {
+    args: {
+        space: new Space(),
+        onConfirm: () => {
+            console.log('Confirm clicked');
+            return null;
+        }
+    }
+};
+
+
+export const ShowsErrorMessageOnConfirm: Story = {
+    args: {
+        space: new Space(),
+        onConfirm: () => {
+            console.log('Confirm clicked');
+            return 'Name already exists';
+        }
+    }
+};
